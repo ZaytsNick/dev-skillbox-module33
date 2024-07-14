5bab6ad9ae3b7e55c3d63122a1338108e4ad11e2
zayts_home <10kcin01@gmail.com>
2024-07-14 21:52:07 +0500

    Task#4 complite

---
+++ Task_4/Task_4.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <numeric>
+//#define TYPE double;
+template < typename T >
+void input(T array[8])
+{
+    std::cout << "Fill the array (8):";
+    for (int i = 0; i < 8; ++i)
+    {
+        std::cin >> array[i];
+    }
+}
+template < typename T >
+T average(T array[8])
+{
+    T sum(0);
+    for (int i = 0; i < 8; ++i)
+    {
+        sum+= array[i];
+    }
+    return sum / 8;
+}
+
+int main()
+{
+    double arr[8];
+    input(arr);
+    std::cout<<"Answer: "<<average(arr);
+}
